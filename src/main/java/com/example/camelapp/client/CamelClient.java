d14c6ea5fa9de77c4eb0b38f0e2788b1beb21b8f
Upendra <upendra@unific.com>
2024-04-03 15:03:46 +0530

    Formatting

--- src/main/java/com/example/camelapp/client/CamelClient.java
+++ src/main/java/com/example/camelapp/client/CamelClient.java
@@ -8,19 +8,17 @@ import org.apache.camel.impl.DefaultCamelContext;
 
 public class CamelClient {
     public static void main(String arg[]) {
-        while(true) {
+//        while(true) {
             callAPI();
-        }
+//        }
     }
 
     private static void callAPI() {
         CamelContext context = new DefaultCamelContext();
         try {
             context.start();
-
             ProducerTemplate producerTemplate = context.createProducerTemplate();
             Exchange exchange = producerTemplate.request("http://localhost:8080/hello", new MyProcessor());
-
             System.out.println("Response from REST endpoint: " + exchange.getMessage().getBody(String.class));
         } finally {
             context.stop();
@@ -30,10 +28,8 @@ public class CamelClient {
     static class MyProcessor implements Processor {
         @Override
         public void process(Exchange exchange) throws Exception {
-            // Set the HTTP method and request body
-            exchange.getIn().setHeader(Exchange.HTTP_METHOD, "GET"); // Set the HTTP method as required
-            exchange.getIn().setHeader(Exchange.CONTENT_TYPE, "application/json"); // Set the content type as required
-//            exchange.getIn().setBody("{'key': 'value'}"); // Set the request body as required
+            exchange.getIn().setHeader(Exchange.HTTP_METHOD, "GET");
+            exchange.getIn().setHeader(Exchange.CONTENT_TYPE, "application/json");
         }
     }
 }
